c7229ed807dd40c3ca3a8ef2901ad931bf4d3af2
Kevin <kmoens@lakeco.com>
2022-08-19 13:25:24 -0500

    initial commit

---
+++ Assets/PupilForce.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PupilForce : MonoBehaviour
+{
+    public int Level = 3;
+    public GameObject Eye;
+    public CircleEdgeCollider2D EyeCollider;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("PupilForce Awake");
+        try
+        {
+            float x = 0, y = 0;
+            GetForce(ref x, ref y, EyeCollider.Radius);
+
+            if (Level <= 1)
+            {
+                gameObject.transform.position = new Vector3(Eye.transform.position.x, Eye.transform.position.y, gameObject.transform.position.z);
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(Level == 1 ? 7 : 6, 0));
+            } else
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(x, Level <= 2 ? 0 : y));
+                GetComponent<Rigidbody2D>().gravityScale = 1f;
+            }
+                
+            
+            //GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            //transform.position = new Vector3(Eye.transform.position.x, Eye.transform.position.y, transform.position.z);
+
+        } catch (System.Exception ex)
+        {
+            Debug.Log("PupilForce Error: " + ex.Message);
+        }
+    }
+    public static void GetForce(ref float x, ref float y, float radius)
+    {
+        var seed = System.DateTime.Now.Millisecond;
+        var seed2 = seed * System.DateTime.Now.Minute;
+        x = GetRandomNumber(seed, 6f, radius * 50f);
+        y = GetRandomNumber(seed2, 6f, radius * 50f);
+        Debug.Log("X:" + x.ToString());
+        Debug.Log("Y:" + y.ToString());
+    }
+    private static float GetRandomNumber(int seed, double minimum, double maximum)
+    {
+        var random = new System.Random(seed);
+        return (float)(random.NextDouble() * (maximum - minimum) + minimum);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/StopPupils.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class StopPupils : MonoBehaviour {
+    public GameObject Eye1;
+    public GameObject pupil1;
+    public CircleEdgeCollider2D EyeCollider1;
+    public GameObject Eye2;
+    public GameObject pupil2;
+    public CircleEdgeCollider2D EyeCollider2;
+    public UnityEngine.UI.Text buttonText;
+    public UnityEngine.UI.Text messageText;
+    public VideoPlayer video;
+    public GameObject videoRenderer;
+    private bool IsPlaying = true;
+    private Vector3 pupil1OrigTransform;
+    private Vector3 pupil2OrigTransform;
+    private float videoTimeRemaining;
+	// Use this for initialization
+	void Start () {
+        pupil1OrigTransform = pupil1.transform.position;
+        pupil2OrigTransform = pupil2.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (videoTimeRemaining <= 0)
+        {
+            videoRenderer.GetComponent<Renderer>().enabled = false;
+        }	else
+        {
+            videoTimeRemaining -= Time.deltaTime;
+        }	
+	}
+    public void PupilStop()
+    {
+        var rbPupil1 = pupil1.GetComponent<Rigidbody2D>();
+        var rbPupil2 = pupil2.GetComponent<Rigidbody2D>();
+        if (IsPlaying)
+        {
+            var dis1 = Eye1.transform.position - pupil1.transform.position;
+            var dis2 = Eye2.transform.position - pupil2.transform.position;
+            Debug.Log(System.Math.Abs(dis1.x).ToString() + ":" + System.Math.Abs(dis1.y).ToString() + ":" + System.Math.Abs(dis2.x).ToString() + ":" + System.Math.Abs(dis2.y).ToString());
+            var total = System.Math.Abs(dis1.x) + System.Math.Abs(dis1.y) + System.Math.Abs(dis2.x) + System.Math.Abs(dis2.y);
+            messageText.text = total.ToString();
+            rbPupil1.constraints = RigidbodyConstraints2D.FreezeAll;
+            rbPupil2.constraints = RigidbodyConstraints2D.FreezeAll;
+            buttonText.text = "Start";
+            videoTimeRemaining = 17f;
+            videoRenderer.GetComponent<Renderer>().enabled = true;
+            video.Play();
+        } else
+        {
+            rbPupil1.constraints = RigidbodyConstraints2D.None;
+            rbPupil2.constraints = RigidbodyConstraints2D.None;
+            pupil1.transform.position = pupil1OrigTransform;
+            pupil2.transform.position = pupil2OrigTransform;
+            float x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+            PupilForce.GetForce(ref x1, ref x1, EyeCollider1.Radius);
+            pupil1.GetComponent<Rigidbody2D>().AddForce(new Vector2(x1, y1));
+            PupilForce.GetForce(ref x2, ref x2, EyeCollider2.Radius);
+            pupil2.GetComponent<Rigidbody2D>().AddForce(new Vector2(x2, y2));
+            buttonText.text = "Stop";
+        }
+        IsPlaying = !IsPlaying;
+    }
+}
